640b005c567923b673b92398c756a944e4db020c
Sujit Kumar <mnamesujit@gmail.com>
2023-04-01 11:27:39 +0530

    All Setup Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './styles/index.css'
+
+import "@fontsource/poppins";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
